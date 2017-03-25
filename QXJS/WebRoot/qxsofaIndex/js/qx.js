724c3a0019c3ad005c47fb3af6864900db449c1a
Clwon1990 <wzp380565621@qq.com>
2017-03-25 22:45:55 +0800

    update

--- QXJS/WebRoot/qxsofaIndex/js/qx.js
+++ QXJS/WebRoot/qxsofaIndex/js/qx.js
@@ -2,22 +2,22 @@
  * Created by cj on 16/3/22.
  */
 $(function(){
-    $(".productSeriesList ul li:nth-child(3n)").css("margin-right","0")
-//    $(".example").imgbox({
-//        'speedIn'		: 0,
-//        'speedOut'		: 0,
-//        'alignment'		: 'center',
-//        'overlayShow'	: true,
-//        'allowMultiple'	: false
-//    });
-//    $(".processDetail ul li a").imgbox({
-//        'speedIn'		: 0,
-//        'speedOut'		: 0,
-//        'alignment'		: 'center',
-//        'overlayShow'	: true,
-//        'allowMultiple'	: false
-//    });
+    $(".productSeriesList ul li:nth-child(3n)").css("margin-right","0");
+    $(".example").imgbox({
+        'speedIn'		: 0,
+        'speedOut'		: 0,
+        'alignment'		: 'center',
+        'overlayShow'	: true,
+        'allowMultiple'	: false
+    });
+    $(".processDetail ul li a").imgbox({
+        'speedIn'		: 0,
+        'speedOut'		: 0,
+        'alignment'		: 'center',
+        'overlayShow'	: true,
+        'allowMultiple'	: false
+    });
 
 
-})
+});
 //字数限制
---
+++ QXJS/WebRoot/qxsofaIndex/js/zzsc.js
@@ -0,0 +1,29 @@
+  $(function() {
+    $('img.image1').data('ad-desc');
+    $('img.image1').data('ad-title');
+    $('img.image4').data('ad-desc');
+    $('img.image5').data('ad-desc');
+    var galleries = $('.ad-gallery').adGallery();
+    $('#switch-effect').change(
+      function() {
+        galleries[0].settings.effect = $(this).val();
+        return false;
+      }
+    );
+    $('#toggle-slideshow').click(
+      function() {
+        galleries[0].slideshow.toggle();
+        return false;
+      }
+    );
+    $('#toggle-description').click(
+      function() {
+        if(!galleries[0].settings.description_wrapper) {
+          galleries[0].settings.description_wrapper = $('#descriptions');
+        } else {
+          galleries[0].settings.description_wrapper = false;
+        }
+        return false;
+      }
+    );
+  });
